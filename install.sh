e00fd3fd6af8d2ed0a1c2886d1804caa644fbfdf
Aoki <zaftzaft7@gmail.com>
2017-07-23 22:30:43 +0900

    add safe update in installer

--- install.sh
+++ install.sh
@@ -3,11 +3,19 @@ cd exporter
 npm install -g
 cd -
 
-mkdir -p /etc/prometheus/netcheck/
-cp ./config/external.json /etc/prometheus/netcheck/default.json
-cp ./config/external.json /etc/prometheus/netcheck/external.json
+if [ ! -d /etc/prometheus/netcheck/ ]; then
+  mkdir -p /etc/prometheus/netcheck/
+  cp ./config/external.json /etc/prometheus/netcheck/default.json
+  cp ./config/external.json /etc/prometheus/netcheck/external.json
+fi
 
 cp ./systemd/netcheck_exporter.service /usr/lib/systemd/system/netcheck_exporter.service
+
+systemctl is-enabled netcheck_exporter
+if [ $? -eq 0 ]; then
+  systemctl stop netcheck_exporter
+fi
+
 
 systemctl daemon-reload
 systemctl enable netcheck_exporter
